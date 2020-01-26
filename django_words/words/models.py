1974cf9cb4e1d14dcf9ee19dd701885bbb2e6a6f
Bartek Olma <bartek@ucreate.it>
2020-01-26 20:43:06 +0100

    Adds simple database models.

--- django_words/words/models.py
+++ django_words/words/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class World(models.Model):
+    world_text = models.CharField(max_length=200)
+    added_date = models.DateTimeField('date added')
+
+class Definition(models.Model):
+    world = models.ForeignKey(World, on_delete=models.CASCADE)
+    definition_text = models.CharField(max_length=500)
+
+    # class PartOfSpeech(models.TextChoices):
+    #     VERB = 'v.';_('verb')
+    # world_part_of_speech = models
