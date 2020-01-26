f57d66727b48a7f8fd0dcf68898686923508d93b
Bartek Olma <bartek@ucreate.it>
2020-01-26 21:28:21 +0100

    Adds models text representation.
    To play in `python manage.py shell`

--- django_words/words/models.py
+++ django_words/words/models.py
@@ -1,12 +1,26 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
+
 
 class Word(models.Model):
     word_text = models.CharField(max_length=200)
-    added_date = models.DateTimeField('date added')
+    added_date = models.DateTimeField("date added")
+
+    def __str__(self):
+        return self.word_text
+
+    def was_added_recently(self):
+        return self.added_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Definition(models.Model):
     word = models.ForeignKey(Word, on_delete=models.CASCADE)
     definition_text = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.definition_text
 
     # class PartOfSpeech(models.TextChoices):
     #     VERB = 'v.';_('verb')
