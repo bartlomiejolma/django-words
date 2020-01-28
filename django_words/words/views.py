0b6da3c103bd55dbb74edea5389861e777883b23
Bartek Olma <bartek@ucreate.it>
2020-01-28 20:57:03 +0100

    Adds view template for words index.

--- django_words/words/views.py
+++ django_words/words/views.py
@@ -1,12 +1,19 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Word
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_word_list = Word.objects.order_by("-added_date")[:5]
+    template = loader.get_template("words/index.html")
+    context = {"latest_word_list": latest_word_list}
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, word_id):
     return HttpResponse(f"You're looking at word {word_id}.")
+
 
 def definitions(request, word_id):
     response = f"You're looking at the definitions of {word_id}."
