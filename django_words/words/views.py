6128ad251ff55c939b4ff952435768ec911aa833
Bartek Olma <bartek@ucreate.it>
2020-01-28 21:04:19 +0100

    Adds details view.

--- django_words/words/views.py
+++ django_words/words/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
 from .models import Word
@@ -11,7 +11,11 @@ def index(request):
 
 
 def detail(request, word_id):
-    return HttpResponse(f"You're looking at word {word_id}.")
+    try:
+        word = Word.objects.get(pk=word_id)
+    except Word.DoesNotExist:
+        raise Http404("Word does not exist")
+    return render(request, 'words/detail.html', {'word': word})
 
 
 def definitions(request, word_id):
