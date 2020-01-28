c014667e6af250e581d540778786ee8c30f1ca8d
Bartek Olma <bartek@ucreate.it>
2020-01-28 21:15:13 +0100

    Adds app name to url.

--- django_words/words/urls.py
+++ django_words/words/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "words"
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:word_id>/', views.detail, name='detail'),
-    path('<int:word_id>/definitions/', views.definitions, name='definition'),
+    path("", views.index, name="index"),
+    path("<int:word_id>/", views.detail, name="detail"),
+    path("<int:word_id>/definitions/", views.definitions, name="definition"),
 ]
