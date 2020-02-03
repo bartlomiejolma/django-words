33f73c6d9fb2453d19700bd37aae83903069ca76
Bartek Olma <bartek@ucreate.it>
2020-02-03 22:24:31 +0100

    Creates a module to fetch words from dictionary.
    Uses Oxford dict. API.
    Fetches data using the sample and stores them in temp structure.
    Credentials are removed from the file. Should be imported from .env
    in the future.

---
+++ django_words/words/utils/oxford_dict.py
@@ -0,0 +1,68 @@
+import time
+
+import requests
+import json
+
+APP_ID = ""
+APP_KEY = ""
+
+
+def request_word(word_id: str):
+
+    LANGUAGE = "en-gb"
+    url = f"https://od-api.oxforddictionaries.com:443/api/v2/entries/{LANGUAGE}/{word_id.lower()}"
+    r = requests.get(url, headers={"app_id": APP_ID, "app_key": APP_KEY})
+    return r.json()
+
+
+def get_definition_data(sense, lexicalEntry):
+    wanted_keys = ["definitions", "examples"]
+    defintion_data = {
+        wanted_key: sense.get(wanted_key, [None])[0] for wanted_key in wanted_keys
+    }
+    defintion_data["phoneticSpelling"] = lexicalEntry.get("pronunciations", [{}])[
+        0
+    ].get("phoneticSpelling", None)
+    return defintion_data
+
+
+def extract_definitions(result):
+    return [
+        get_definition_data(sense=sense, lexicalEntry=lexicalEntry)
+        for lexicalEntry in result["lexicalEntries"]
+        for entry in lexicalEntry["entries"]
+        for sense in entry["senses"]
+    ]
+
+
+def extract_relevant_data(oxford_results: dict):
+    relevant_word_keys = ["word", "type"]
+    words_definitions = []
+    for result in oxford_results.get("results", []):
+        word_data = {
+            relevant_word_key: result[relevant_word_key]
+            for relevant_word_key in relevant_word_keys
+        }
+        definitions = extract_definitions(result)
+        words_definitions.append((word_data, definitions))
+    return words_definitions
+
+
+def request_data_about_line(valid_word_line):
+    word = valid_word_line.split(" - ")[0]
+    print(f"{word}")
+    time.sleep(1)
+    r = request_word(word)
+    return extract_relevant_data(r)
+
+
+def get_data_from_file(words_file_path):
+    with open(words_file_path, "r", encoding="utf-8") as words_file:
+        word_lines = words_file.readlines()
+        valid_word_lines = [
+            word_line for word_line in word_lines if word_line[0].isalnum()
+        ]
+        return [
+            request_data_about_line(valid_word_line)
+            for valid_word_line in valid_word_lines
+        ]
