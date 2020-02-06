e501db4f144974f28cbcd67cb12e604160f157cf
Bartek Olma <bartek@ucreate.it>
2020-02-06 18:44:01 +0100

    Adds common interface for RandomSelectable data.

--- django_words/words/models.py
+++ django_words/words/models.py
@@ -5,7 +5,18 @@ from django.db import models
 from django.utils import timezone
 
 
-class Word(models.Model):
+class RandomSelectable(models.Model):
+    @classmethod
+    def random(cls):
+        count = cls.objects.count()
+        random_index = random.randint(1, count)
+        return cls.objects.get(pk=random_index)
+
+    class Meta:
+        abstract = True
+
+
+class Word(RandomSelectable):
     word_text = models.CharField(max_length=200)
     added_date = models.DateTimeField("date added")
     word_class = models.CharField(max_length=200, default="")
@@ -16,14 +27,8 @@ class Word(models.Model):
     def was_added_recently(self):
         return self.added_date >= timezone.now() - datetime.timedelta(days=1)
 
-    @classmethod
-    def random(cls):
-        count = cls.objects.count()
-        random_index = random.randint(1, count)
-        return cls.objects.get(pk=random_index)
 
-
-class Definition(models.Model):
+class Definition(RandomSelectable):
     word = models.ForeignKey(Word, on_delete=models.CASCADE)
     definition_text = models.CharField(max_length=500)
     example = models.CharField(max_length=500, default="", null=True)
