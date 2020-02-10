fece0dc18c04aacae3543b85f55021a5e58bbfa7
Bartek Olma <bartek@ucreate.it>
2020-02-10 20:50:36 +0100

    Uses generic views.

--- django_words/words/urls.py
+++ django_words/words/urls.py
@@ -4,8 +4,8 @@ from .views import list_words, exercises
 
 app_name = "words"
 urlpatterns = [
-    path("list", list_words.index, name="index"),
-    path("list/<int:word_id>/", list_words.detail, name="detail"),
+    path("list", list_words.IndexView.as_view(), name="index"),
+    path("list/<int:pk>/", list_words.DetailView.as_view(), name="detail"),
     path("exercises", exercises.index, name="exercises"),
     path("exercises/<int:exercise_id>/result", exercises.result, name="exercises_results"),
     path("exercises/<int:exercise_id>/verify", exercises.verify, name="exercises_verify")
--- django_words/words/views/list_words.py
+++ django_words/words/views/list_words.py
@@ -1,15 +1,19 @@
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
 from ..models import Word
 
 
-def index(request):
-    latest_word_list = Word.objects.order_by("-added_date")[:5]
-    context = {"latest_word_list": latest_word_list}
-    return render(request, "words/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "words/index.html"
+    context_object_name = "latest_word_list"
+
+    def get_queryset(self):
+        """Return the last five added words."""
+        return Word.objects.order_by("-added_date")[:5]
 
 
-def detail(request, word_id):
-    word = get_object_or_404(Word, pk=word_id)
-    return render(request, "words/detail.html", {"word": word})
+class DetailView(generic.DetailView):
+    model = Word
+    template_name = "words/detail.html"
