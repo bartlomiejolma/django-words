ec3daa8e7cf0b72e8f5c830b836236a9eea6ed87
Bartek Olma <bartek@ucreate.it>
2020-02-10 22:24:52 +0100

    Adds simple test.

--- django_words/words/tests.py
+++ django_words/words/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Word
+
+
+class WordModelTests(TestCase):
+
+    def test_was_added_recently_with_future_word(self):
+        """
+        was_added_recently() returns False for word whose pub_date
+        is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_word = Word(added_date=time)
+        self.assertIs(future_word.was_added_recently, False)
