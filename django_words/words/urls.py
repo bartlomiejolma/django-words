d4f6ed80fc7c2a0c9dbfffc2272e99ece4cd9560
Bartek Olma <bartek@ucreate.it>
2020-02-18 21:13:17 +0100

    Adds template and url to add words as file or list

--- django_words/words/urls.py
+++ django_words/words/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import list_words, exercises
+from .views import list_words, exercises, add_words
 
 app_name = "words"
 urlpatterns = [
+    path("batch", add_words.add, name="add_words"),
+    path("batch/add", add_words.batch, name="add_batch"),
     path("list", list_words.IndexView.as_view(), name="index"),
     path("list/<int:pk>/", list_words.DetailView.as_view(), name="detail"),
     path("exercises", exercises.index, name="exercises"),
