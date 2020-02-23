15c9dc51180570c3d82aab7e3fd6deea5a8cc34d
Bartek Olma <bartek@ucreate.it>
2020-02-23 15:07:31 +0100

    Adds more comprehensive tests.

--- django_words/words/tests.py
+++ django_words/words/tests.py
@@ -7,12 +7,31 @@ from .models import Word
 
 
 class WordModelTests(TestCase):
-
     def test_was_added_recently_with_future_word(self):
         """
-        was_added_recently() returns False for word whose pub_date
+        was_added_recently() returns False for word whose added_date
         is in the future.
         """
         time = timezone.now() + datetime.timedelta(days=30)
         future_word = Word(added_date=time)
         self.assertIs(future_word.was_added_recently(), False)
+
+    def test_was_added_recently_with_words_older_than_1_day(self):
+        """
+        was_added_recently() returns False for word whose added_date
+        older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        future_word = Word(added_date=time)
+        self.assertIs(future_word.was_added_recently(), False)
+
+    def test_was_added_recently_with_word_within_1_day(self):
+        """
+        was_added_recently() returns True for word whose added_date
+        is within 1 day from now.
+        """
+        time = timezone.now() - (
+            datetime.timedelta(days=1) - datetime.timedelta(seconds=1)
+        )
+        future_word = Word(added_date=time)
+        self.assertIs(future_word.was_added_recently(), True)
